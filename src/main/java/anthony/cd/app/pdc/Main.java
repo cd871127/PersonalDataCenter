6dbd589a75f0c795a96f47c572f8110fe1667beb
Anthony <cdistc@163.com>
2017-11-23 00:56:24 +0800

    add mybatis

--- src/main/java/anthony/cd/app/pdc/Main.java
+++ src/main/java/anthony/cd/app/pdc/Main.java
@@ -1,11 +1,12 @@
 package anthony.cd.app.pdc;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("")
 public class Main {
-
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
--- src/main/java/anthony/cd/app/pdc/TestController.java
+++ src/main/java/anthony/cd/app/pdc/TestController.java
@@ -3,14 +3,24 @@ package anthony.cd.app.pdc;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletRequest;
+
 @RestController
 @RequestMapping("/")
 public class TestController {
 
     @RequestMapping("test")
-    public String testController() {
-        System.out.println(123);
-        return "123";
+    public String testController(ServletRequest request) {
+        System.out.println(124367);
+
+        return "1231";
+    }
+
+    @RequestMapping("test1")
+    public String testController2() {
+        System.out.println(1243678);
+
+        return "1234";
     }
 
 }
