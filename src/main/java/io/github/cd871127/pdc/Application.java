f74effbe8fe7263a4f1f9f8b8cb0a79fb71880e4
Anthony <cdistc@163.com>
2018-06-01 17:45:28 +0800

    1

--- src/main/java/io/github/cd871127/pdc/Application.java
+++ src/main/java/io/github/cd871127/pdc/Application.java
@@ -4,12 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @MapperScan("io.github.cd871127.pdc.*.mapper")
 @ServletComponentScan("io.github.cd871127.pdc.common.filter")
-@EnableSwagger2
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
